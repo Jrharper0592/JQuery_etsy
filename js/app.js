fcd26a0a9bbf3c5411384a0e59e8fa9d6bb7039d
Jonathan <jrhaccount@yahoo.com>
2015-02-02 11:39:46 -0600

    etsy Project

--- js/app.js
+++ js/app.js
@@ -11,10 +11,12 @@ function app(){
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
         {url: "./bower_components/lodash/dist/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/backbone/backbone.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
     })
 
 }
+
+var token = "ljn6cl37cqrlmiy8hbnq63ww"
