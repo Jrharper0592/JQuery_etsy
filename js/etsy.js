c2b7ff8baf68dae1212ad6b058086ae699aa7f35
Jonathan <jrhaccount@yahoo.com>
2015-02-04 23:17:20 -0600

    90% complete

--- js/etsy.js
+++ js/etsy.js
@@ -1,54 +1,93 @@
-;(function(){
-"use strict";
-    function EtsyClient(token){
+;(function() {
+    "use strict";
+
+    function EtsyClient(token) {
         this.token = token;
         var self = this;
         console.log("winning");
+
+        var EtsyRouter = Backbone.Router.extend({
+            routes: {
+                "": "search",
+                "listing/:listing_id": "details",
+                "search/:keywords": "search"
+            },
+            search: function(keywords) {
+                self.draw(keywords);
+
+            },
+            details: function(listing_id) {
+                self.drawDetails(listing_id);
+            },
+            initialize: function() {
+                Backbone.history.start();
+            }
+          
+            
+        });
+        var Router = new EtsyRouter();
+
+        this.handleEvents();
     }
 
     EtsyClient.prototype = {
-        URLs: { 
-            listings: "https://openapi.etsy.com/v2/listings/"
+        URLs: {
+            listings: "https://openapi.etsy.com/v2/listings/active"
         },
-    
-        access_token: function(){
-            return  ".js"+"?access_token="+this.token+"&callback=?";
+
+        handleEvents: function(){
+            $('body').on("submit", "form", function(event) {
+                event.preventDefault();
+                window.location.hash = 'search/' + this.querySelector('input').value;
+            });
         },
-        //write in a $.ajax() Request for etsy info.
-        // getlisting: function()
-// //         /**
-// //          * getData
-// //          * @arguments none.
-// //          * @return promise
-// //          */
-// //       
-            getData: function(){
-             var x = $.Deferred();
 
-            if(this.length > 0){
-                 x.resolve(this.listings);
-             } else {
-                var p = $.get(this.URLs.listings + this.access_token());
-                 p.then(function(data){
-                     x.resolve(data);
-                     this.listings = data;
-                 });
-             }
-
-             return x;
-         },
-
-
-        draw: function(){
-           
+        access_token: function() {
+            return "?api_key=" + this.token;
         },
-        //add into a function
-        $.when(
-            //1-get api data 
-            //2-load html from templates/
-            ).then(function(data,template){
-            console.log("you got this!");
-            })
+
+        getData: function(keywords) {
+            if (keywords) {
+                keywords = "&keywords=" + keywords;
+            } else {
+                keywords = "";
+            }
+            var etsydata = $.getJSON(
+                "https://openapi.etsy.com/v2/listings/active.js?includes=Images" + keywords + "&api_key=ljn6cl37cqrlmiy8hbnq63ww&callback=?"
+            ).then(function(jsonp) {
+                return jsonp;
+            });
+
+            return etsydata;
+        },
+        drawDetails: function(listing_id){
+            $.when(
+                this.getData(listing_id),
+                this.loadTemplate("detailed")
+                ).then(function(apidata, template){
+                    var templatingFun = _.template(template);
+                    document.querySelector(".container").innerHTML = templatingFun(apidata);
+                });
+        },
+        
+        loadTemplate: function(name) {
+            return $.get("./templates/" + name + ".html").then(function(data) {
+                return data;
+            });
+        },
+
+        draw: function(keywords) {
+            $.when(
+                //1-get api data 
+                this.getData(keywords),
+                //2-load html from templates/
+                this.loadTemplate("home")
+            ).then(function(apidata, template) {
+                var templatingFn = _.template(template);
+                document.querySelector(".container").innerHTML = templatingFn(apidata);
+
+            });
+        },
         
     };
 
